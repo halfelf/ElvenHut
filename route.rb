86a5e648916086d4317e212405fc788dc1f72f1d
Shu Wang <wangshu@admaster.com.cn>
2012-07-18 11:15:36 +0800

    set layout and custom index page in "route.rb". Add layout and default index page in /views

--- route.rb
+++ route.rb
@@ -2,9 +2,20 @@
 
 require "sinatra"
 require "rdiscount"
+require "erb"
+
+root = File.dirname(__FILE__)
+view_path = root + "/views/"
+public_path = root + "/public/"
+
+set :markdown, :layout_engine => :erb, :layout => :background
 
 get "/" do 
-    markdown :index
+    if File.exist?(view_path + "my_index.md")
+        markdown :my_index
+    else
+        markdown :index
+    end
 end
 
 
---
+++ views/index.md
@@ -0,0 +1,4 @@
+This is a Blog Engine for personal using.
+=========================================
+
+Edit /views/my_index.md for your own index page.
