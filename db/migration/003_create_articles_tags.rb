eb679e09d30483e1491e6a229dc5a100fdd662b1
wyvernbai <wyvern.bai@gmail.com>
2012-09-18 20:17:19 +0800

    tab=2

--- db/migration/003_create_articles_tags.rb
+++ db/migration/003_create_articles_tags.rb
@@ -3,10 +3,10 @@ require 'sequel'
 require 'mysql2'
 
 Sequel.migration do
-	change do
-		create_table(:articles_tags) do
-    		foreign_key :article_id
-    		foreign_key :tag_id
-    	end
+  change do
+    create_table(:articles_tags) do
+      foreign_key :article_id
+      foreign_key :tag_id
     end
+  end
 end
